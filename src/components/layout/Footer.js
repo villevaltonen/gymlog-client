8b3a7936e63db1489176d164b70aa0a87e9613c5
Ville Valtonen <valtonen.vt@gmail.com>
2021-01-23 15:09:34 +0200

    - styled component footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  display: grid;
+  justify-content: center;
+  backround-color: white;
+  font-family: Arial;
+  font-size: 14px;
+  margin-top: 20px;
+`;
 
 const Navbar = () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
+    <StyledFooter>
       Copyright &copy; {new Date().getFullYear()} Ville Valtonen
-    </footer>
+    </StyledFooter>
   );
 };
 
